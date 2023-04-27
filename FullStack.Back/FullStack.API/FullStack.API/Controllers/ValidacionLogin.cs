1d25639bf8a3639e6cdbdfcc40218981ed3e7a6c
AndresMoreta20 <andresmoreta2020@gmail.com>
2023-04-27 16:25:50 -0500

    se anade el backend de la aplicacion

---
+++ FullStack.Back/FullStack.API/FullStack.API/Controllers/ValidacionLogin.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FullStack.API.Controllers
+{
+    public class ValidacionLogin : Controller
+    {
+        private readonly ILogger<ValidacionLogin> _logger;
+
+        public ValidacionLogin(ILogger<ValidacionLogin> logger)
+        {
+            _logger = logger;
+        }
+        //http://apiservicios.ecuasolmovsa.com:3009/api/Usuarios?usuario=5001&password=5001u
+        [HttpGet(Name = "getUser")]
+
+        public async Task<string> Get(string usuario, string password)
+        {
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    var url = "http://apiservicios.ecuasolmovsa.com:3009/api/Usuarios?usuario=" + usuario + "&password=" + password;
+
+                
+                    HttpResponseMessage response = await httpClient.GetAsync(url);
+
+                    string responseBody = await response.Content.ReadAsStringAsync();
+
+                    Console.WriteLine(responseBody);
+                    return responseBody;
+                }
+                //return "Holi";
+            }
+            catch (Exception error)
+            {
+                return ("erooor: " + error);
+            }
+        }
+
+        [HttpGet("Emisores")]
+
+        public async Task<string> Get()
+        {
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    var url = "http://apiservicios.ecuasolmovsa.com:3009/api/Varios/GetEmisor";
+
+                    HttpResponseMessage response = await httpClient.GetAsync(url);
+
+                 
+                    string responseBody = await response.Content.ReadAsStringAsync();
+
+                 
+                    Console.WriteLine(responseBody);
+                    return responseBody;
+                }
+                
+            }
+            catch (Exception error)
+            {
+                return ("erooor: " + error);
+            }
+        }
+
+   
+    }
+}
